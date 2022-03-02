6f8c78bce7cff31fcec64192be0df37257161ba5
razumolla <rajuahmed9344@gmail.com>
2022-03-02 19:31:41 +0600

    update errer

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ const displayAllPhone = (phones) => {
     for (const phone of first20Phone) {
         // console.log(phone);
         const div = document.createElement('div');
-        div.className = "col-lg-4 col-md-6 col-sm-12 mb-3";
+        div.className = "col-lg-4  col-sm-12 mb-3";
         div.innerHTML = `
-            <div class="card" style="width: 18rem;">
+            <div class="card w-100">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Name: ${phone.phone_name}  </h5>
@@ -60,35 +60,35 @@ const phoneDetails = (id) => {
 }
 
 const showPhonedetails = (phone) => {
-
     console.log(phone);
+
     const div = document.createElement('div')
     details.innerHTML = ''
     div.innerHTML = `
-        <div class="card mb-3  mx-auto w-100 border border-success bg-transparent" >
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${phone.image}" class="card-img-top" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                    <h4 class="card-title">Brand: ${phone.brand}  </h4>
-                    <h4 class="card-title">Name: ${phone.name}  </h4>
-                    <p class="card-title"> <strong>ChipSet: </strong> ${phone.mainFeatures.chipSet}</p>
-                    <p class="card-title"> <strong>Display-Size: </strong> ${phone.mainFeatures.displaySize}</p>
-                    <p class="card-title"><strong>Memory: </strong> ${phone.mainFeatures.memory} </p
-                    <p class="card-title"><strong>Sensor: </strong> ${phone.mainFeatures.sensors} </p
-                    <p class="card-title"><strong> Bluetooth: </strong> ${phone.others.Bluetooth} </p
-                    <p class="card-title"><strong> GPS: </strong> ${phone.others.GPS} </p
-                    <p class="card-title"><strong> NFC: </strong> ${phone.others.NFC} </p
-                    <p class="card-title"><strong> Radio: </strong> ${phone.others.Radio} </p
-                    <p class="card-title"><strong> USB: </strong> ${phone.others.USB} </p
-                    <p class="card-title"><strong> WLAN: </strong> ${phone.others.WLAN} </p
-                    <p class="card-title"><strong>Release-Date: </strong> ${phone.releaseDate ? phone.releaseDate : 'No release date found!'}  </h5>
+            <div class="card mb-3  mx-auto w-100 border border-success bg-transparent" >
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${phone.image}" class="card-img-top" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h4 class="card-title">Brand: ${phone.brand}  </h4>
+                        <h4 class="card-title">Name: ${phone.name}  </h4>
+                        <p class="card-title"> <strong>ChipSet: </strong> ${phone.mainFeatures.chipSet}</p>
+                        <p class="card-title"> <strong>Display-Size: </strong> ${phone.mainFeatures.displaySize}</p>
+                        <p class="card-title"><strong>Memory: </strong> ${phone.mainFeatures.memory} </p
+                        <p class="card-title"><strong>Sensor: </strong> ${phone.mainFeatures.sensors} </p
+                        <p class="card-title"><strong> Bluetooth: </strong> ${phone.others.Bluetooth ? phone.others.Bluetooth : 'none'} </p
+                        <p class="card-title"><strong> GPS: </strong> ${phone.others.GPS ? phone.others.GPS : 'none'} </p
+                        <p class="card-title"><strong> NFC: </strong> ${phone.others.NFC ? phone.others.NFC : 'none'} </p
+                        <p class="card-title"><strong> Radio: </strong> ${phone.others.Radio ? phone.others.Radio : 'none'} </p
+                        <p class="card-title"><strong> USB: </strong> ${phone.others.USB ? phone.others.USB : 'none'} </p
+                        <p class="card-title"><strong> WLAN: </strong> ${phone.others.WLAN ? phone.others.WLAN : 'none'} </p
+                        <p class="card-title"><strong>Release-Date: </strong> ${phone.releaseDate ? phone.releaseDate : 'No release date found!'}  </h5>
+                        </div >
                     </div >
                 </div >
             </div >
-        </div >
-    `;
+        `;
     details.appendChild(div)
 }
